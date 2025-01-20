2bbb59ab5a2668d643d357452e5d0993739773af
Paola Nascimento <paola.rebeca.ldn@gmail.com>
2025-01-20 20:32:24 -0300

    correções finais

--- src/knapsack.py
+++ src/knapsack.py
@@ -1,22 +1,18 @@
 def knapsack(peso_maximo, itens):
-    n = len(itens)
-    dp = [[0] * (peso_maximo + 1) for _ in range(n + 1)]
 
-    for i in range(1, n + 1):
-        nome, peso, valor = itens[i - 1]
-        for w in range(peso_maximo + 1):
-            if peso <= w:
-                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - peso] + valor)
-            else:
-                dp[i][w] = dp[i - 1][w]
+    itens_ordenados = sorted(itens, key=lambda item: item[2] / item[1], reverse=True)
 
-    valor_total = dp[n][peso_maximo]
+    peso_atual = 0
+    valor_total = 0
     itens_escolhidos = []
-    w = peso_maximo
 
-    for i in range(n, 0, -1):
-        if dp[i][w] != dp[i - 1][w]:
-            itens_escolhidos.append(itens[i - 1])
-            w -= itens[i - 1][1]
-    
+    for nome, peso, valor in itens_ordenados:
+        if peso_atual + peso <= peso_maximo:  
+            itens_escolhidos.append((nome, peso, valor))
+            peso_atual += peso
+            valor_total += valor
+        else:
+            break
+
     return valor_total, itens_escolhidos
+
